699b9274dadb492ef54297acf90601bb834e0eb9
Sasmith Manawadu <sasmithgit@gmail.com>
2024-11-18 20:20:52 +0530

    Added logger statements to EquipmentController for improved error tracking and process monitoring | modified

--- src/main/java/lk/sasax/GreenShadow/controller/EquipmentController.java
+++ src/main/java/lk/sasax/GreenShadow/controller/EquipmentController.java
@@ -7,6 +7,8 @@ import lk.sasax.GreenShadow.exception.EquipmentNotFoundException;
 import lk.sasax.GreenShadow.exception.StaffNotFoundException;
 import lk.sasax.GreenShadow.service.EquipmentService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,17 +25,22 @@ public class EquipmentController {
     @Autowired
     private final EquipmentService equipmentService;
 
+    private static final Logger logger = LoggerFactory.getLogger(EquipmentController.class);
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveEquipment(@RequestBody EquipmentDTO equipmentDTO) {
         try{
             System.out.println(equipmentDTO);
             equipmentService.saveEquipment(equipmentDTO);
+            logger.info("Equipment saved successfully");
             return ResponseEntity.status(HttpStatus.CREATED).build();
         }catch (DataPersistFailedException e){
             e.printStackTrace();
+            logger.error("Request failed with status: BAD_REQUEST due to invalid input or processing error");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }catch (Exception e){
             e.printStackTrace();
+            logger.error("Cannot save equipment");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -44,14 +51,18 @@ public class EquipmentController {
     public ResponseEntity<Void> updateEquipment(@PathVariable("id") String id, @RequestBody EquipmentDTO equipmentDTO) {
         try{
             equipmentService.updateEquipment(id,equipmentDTO);
+            logger.info("Equipment updated successfully");
             return ResponseEntity.status(HttpStatus.OK).build();
         }catch (EquipmentNotFoundException e){
+            logger.error("Equipment not found");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (DataPersistFailedException e){
+            logger.error("Request failed with status: BAD_REQUEST due to invalid input or processing error");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }catch (Exception e){
+            logger.error("Cannot update equipment");
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
@@ -61,12 +72,16 @@ public class EquipmentController {
     public ResponseEntity<Void> deleteEquipment(@PathVariable("id") String id) {
         try{
             equipmentService.deleteEquipment(id);
+            logger.info("Equipment deleted successfully");
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }catch (IllegalStateException e){
+            logger.error("Request failed with status: BAD_REQUEST due to invalid input or processing error");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }catch (StaffNotFoundException e){
+            logger.error("Equipment not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("Cannot delete equipment");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -75,10 +90,13 @@ public class EquipmentController {
     public EquipmentResponse getSelectedEquipment(@PathVariable("id") String id) {
         try{
             EquipmentResponse equipmentResponse = equipmentService.getSelectedEquipment(id);
+            logger.info("Equipment selected successfully");
             return ResponseEntity.status(HttpStatus.OK).body(equipmentResponse).getBody();
         }catch (EquipmentNotFoundException e){
+            logger.error("Equipment not found");
             return (EquipmentResponse) ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("Cannot select equipment");
             return (EquipmentResponse) ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -87,10 +105,13 @@ public class EquipmentController {
     public ResponseEntity<List<EquipmentDTO>> getAllEquipment() {
         try{
             List<EquipmentDTO> equipments = equipmentService.getAllEquipments();
+            logger.info("Equipment list selected successfully");
             return ResponseEntity.status(HttpStatus.OK).body(equipments);
         }catch (EquipmentNotFoundException e){
+            logger.error("Equipment not found");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }catch (Exception e){
+            logger.error("Cannot select equipments");
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
